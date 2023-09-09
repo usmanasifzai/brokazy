d443764df630f69bb57129a83128ac03dbda5542
Usman Asif <usman.asif.zai@gmail.com>
2023-09-09 15:40:24 -0700

    Subscription: Added Test Cases for Card-Details Screen

---
+++ __tests__/Card-details.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import CardDetails from '../src/screens/card-details/card-details';
+import SuccessModal from '../src/components/success-modal/success-modal';
+jest.useFakeTimers();
+
+const mockNavigation = {
+  navigate: jest.fn(),
+};
+
+let modalVisible = false;
+
+const setIsModalVisible = () => {
+  modalVisible = true;
+};
+
+describe('CardDetails component', () => {
+  it('renders the CardDetails component', () => {
+    const {getByText} = render(<CardDetails navigation={mockNavigation} />);
+
+    expect(getByText('CARD DETAILS')).toBeTruthy();
+  });
+
+  it('displays success modal when Continue button is pressed and navigate and home when success modal button is clicked', async () => {
+    const {getByTestId, queryByText} = render(
+      <>
+        <CardDetails navigation={mockNavigation} />
+        <SuccessModal
+          closeModal={setIsModalVisible}
+          modalVisible={modalVisible}
+          navigation={mockNavigation}
+        />
+      </>,
+    );
+
+    expect(queryByText('SUCCESS!')).toBeNull();
+    fireEvent.press(getByTestId('CONTINUE'));
+
+    const btn = getByTestId('BUTTON');
+    expect(btn).toBeTruthy();
+
+    setIsModalVisible();
+
+    expect(queryByText('SUCCESS!')).toBeTruthy();
+    fireEvent.press(btn);
+    expect(mockNavigation.navigate).toHaveBeenCalledWith('Home');
+  });
+});
