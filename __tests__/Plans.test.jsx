0a82c22ffa6cccb4d23fc34f54b02bc2a4705e9c
Usman Asif <usman.asif.zai@gmail.com>
2023-09-09 15:41:39 -0700

    Subscription: Added Test Cases for Plans Screen

---
+++ __tests__/Plans.test.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import Plans from '../src/screens/plans/Plans';
+import PlanModal from '../src/components/plan-modal/plan-modal';
+jest.useFakeTimers();
+
+const mockNavigation = {
+  navigate: jest.fn(),
+};
+
+let isVisible = false;
+
+const setIsVisible = () => {
+  isVisible = true;
+};
+
+describe('Plans component', () => {
+  it('displays premium plan details and activates/update plan button', () => {
+    const {getByText, getByTestId} = render(
+      <>
+        <Plans navigation={mockNavigation} />
+        <PlanModal
+          setModalVisible={setIsVisible}
+          modalVisible={isVisible}
+          planDetails={{}}
+          navigation={mockNavigation}
+        />
+      </>,
+    );
+
+    expect(getByText('Subscriptions Plans')).toBeTruthy();
+
+    expect(getByText('PREMIUM PLAN')).toBeTruthy();
+    expect(getByText('54 Days Left')).toBeTruthy();
+    expect(getByText('May 22, 2022')).toBeTruthy();
+
+    const activateUpdateButton = getByText('ACTIVATE/UPDATE PLAN');
+    expect(activateUpdateButton).toBeTruthy();
+    expect(activateUpdateButton).toBeDisabled();
+
+    const planCard = getByTestId('plan-card0');
+    fireEvent.press(planCard);
+    setIsVisible();
+
+    const subBtn = getByTestId('SUBSCRIBE');
+    fireEvent.press(subBtn);
+
+    fireEvent.press(activateUpdateButton);
+
+    expect(activateUpdateButton).not.toBeDisabled();
+  });
+});
