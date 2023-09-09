1895cf649fc1474f7667905171619d937d0f6538
Usman Asif <usman.asif.zai@gmail.com>
2023-09-08 18:06:04 -0700

    Main App: Header Component back icon

---
+++ src/components/header-with-back-icon/header-with-back-icon.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {TouchableRipple} from 'react-native-paper';
+import {useNavigation} from '@react-navigation/native';
+import ArrowBack from '../../assets/svgs/ArrowBack';
+
+export default function HeaderWithBackIcon(props) {
+  const navigations = useNavigation();
+
+  return (
+    <View style={styles.view}>
+      <View style={styles.header}>
+        <TouchableRipple
+          borderless
+          style={styles.ripple}
+          onPress={() => navigations.goBack()}
+          rippleColor={'#8493AE20'}>
+          <ArrowBack fill={'white'} />
+        </TouchableRipple>
+        <Text style={[styles.title]}>{props.title}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    height: 50,
+    width: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: '#032726',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    borderBlockColor: '#1b3538',
+    borderBottomWidth: 1,
+  },
+  ripple: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 17,
+    textAlign: 'center',
+    width: '70%',
+    fontWeight: '400',
+    color: 'white',
+  },
+});
