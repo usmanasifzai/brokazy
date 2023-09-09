34ecfb1d2d39047feb19c359bf16733fb502cd26
Usman Asif <usman.asif.zai@gmail.com>
2023-09-09 15:39:22 -0700

    Main App: Setup for Testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.test.js$': 'babel-jest',
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@react-native*|react-native|one-weir-mobile|react-native-config|@react-navigation|rn-fetch-blob|@react-native-async-storage/async-storage|react-native-nfc-manager|react-native-gesture-handler)|/).*',
+  ],
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
